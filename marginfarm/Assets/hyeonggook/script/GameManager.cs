6af37901cca283887d8f203186af2a4d64f34320
HyeongGook <khg5632@gmail.com>
2022-01-12 02:29:46 +0900

    gamemanager, 말선택창 1차

---
+++ marginfarm/Assets/hyeonggook/script/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct HorseInfo
+{
+    public string name;
+    public int key;
+    public int level;
+    public int speed;
+    public int accel;
+    public int hp;
+    public int agility;
+    public int consis;
+    public int items;
+    public bool alive;
+
+    public HorseInfo (string name, int key, int level, int speed, int accel, int hp, int agility, int consis, int items, bool alive)
+    {
+        this.name = name;
+        this.key = key;
+        this.level = level;
+        this.speed = speed;
+        this.accel = accel;
+        this.hp = hp;
+        this.agility = agility;
+        this.consis = consis;
+        this.items = items;
+        this.alive = alive;
+    }
+   
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int many;
+
+    public HorseInfo[] UserHorse = new HorseInfo[]
+    {
+        new HorseInfo ("적토마", 2, 1, 15, 7, 100, 20, 50, 0, true),
+        new HorseInfo ("", 0, 0, 15, 7, 100, 20, 50, 0, false),
+        new HorseInfo ("", 0, 0, 15, 7, 100, 20, 50, 0, false),
+        new HorseInfo ("", 0, 0, 15, 7, 100, 20, 50, 0, false),
+        new HorseInfo ("", 0, 0, 15, 7, 100, 20, 50, 0, false),
+        new HorseInfo ("", 0, 0, 15, 7, 100, 20, 50, 0, false),
+    };
+
+    void Awake()
+    {
+        /*
+        for (int i = 0; i > 6; i++)
+        {
+            if (UserHorse[i].alive == true)
+            {
+                many = i + 1;
+            }
+            else
+            {
+                break;
+            }
+        }*/
+
+        many = 1;
+
+        instance = this;
+    }
+}
+
---
+++ marginfarm/Assets/hyeonggook/script/ListSetting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListSetting : MonoBehaviour
+{
+
+    public Button button;
+    public GameObject x1;
+    public GameObject x2;
+
+    public int check;
+    public int con;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        check = GameManager.instance.many;
+
+        for (con = 1; con <= 6; con++)
+        {
+            button = GameObject.Find("horse" + con.ToString()).GetComponent<Button>();
+            GameObject x1 = GameObject.Find("onex" + con.ToString());
+            GameObject x2 = GameObject.Find("twox" + con.ToString());
+
+            if (check >= con)
+            {
+                button.interactable = true;
+                x1.SetActive(false);
+                x2.SetActive(false);
+            }
+            else
+            {
+                button.interactable = false;
+                x1.SetActive(true);
+                x2.SetActive(true);
+            };
+
+            //Debug.Log(GameManager.instance.UserHorse[con - 1].level);
+        };
+    
+    }
+
+    // Update is called once per frame
+
+    
+}
