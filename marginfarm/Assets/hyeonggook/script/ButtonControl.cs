0b51b317c52d68a3a3501879dd608693f8d9d7f2
HyeongGook <khg5632@gmail.com>
2022-01-13 02:37:17 +0900

    말속성창, 메인-농장 연결

---
+++ marginfarm/Assets/hyeonggook/script/ButtonControl.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonControl : MonoBehaviour
+{
+    public GameObject spec;
+    public Image bar;
+    public Text gauge;
+    public Text name_t;
+
+    public int data;
+    public int i;
+    public int[] userdata = new int[5];
+
+    public void specview_close()
+    {
+        if (GameManager.instance.spec_check == true)
+        {
+            GameObject spec = GameObject.Find("spec");
+            iTween.MoveTo(spec, iTween.Hash("y", 1100, "delay", 0.1f, "time", 0.5f));
+            GameManager.instance.spec_check = false;
+        }
+    }
+    public void specview_h1()
+    {
+        GameManager.instance.select = 0;
+        data = GameManager.instance.select;
+
+        name_t = GameObject.Find("name_t").GetComponent<Text>();
+        name_t.text = GameManager.instance.UserHorse[data].name;
+
+        userdata[0] = GameManager.instance.UserHorse[data].speed;
+        userdata[1] = GameManager.instance.UserHorse[data].accel;
+        userdata[2] = GameManager.instance.UserHorse[data].hp;
+        userdata[3] = GameManager.instance.UserHorse[data].agility;
+        userdata[4] = GameManager.instance.UserHorse[data].consis;
+
+        if (GameManager.instance.spec_check == false)
+        {
+            GameObject spec = GameObject.Find("spec");
+            iTween.MoveTo(spec, iTween.Hash("y", 470, "delay", 0.1f, "time", 0.5f));
+        }
+        GameManager.instance.spec_check = true;
+
+        for (i = 1; i < 6; i++)
+        {
+            bar = GameObject.Find("gauge" + i.ToString()).GetComponent<Image>();
+            bar.fillAmount = userdata[i - 1] / 100f;
+            gauge = GameObject.Find("gauge_t" + i.ToString()).GetComponent<Text>();
+            gauge.text = userdata[i - 1].ToString() + "/ 100";
+        }
+    }
+    public void specview_h2()
+    {
+        GameManager.instance.select = 1;
+        data = GameManager.instance.select;
+
+        name_t = GameObject.Find("name_t").GetComponent<Text>();
+        name_t.text = GameManager.instance.UserHorse[data].name;
+
+        userdata[0] = GameManager.instance.UserHorse[data].speed;
+        userdata[1] = GameManager.instance.UserHorse[data].accel;
+        userdata[2] = GameManager.instance.UserHorse[data].hp;
+        userdata[3] = GameManager.instance.UserHorse[data].agility;
+        userdata[4] = GameManager.instance.UserHorse[data].consis;
+
+        if (GameManager.instance.spec_check == false)
+        {
+            GameObject spec = GameObject.Find("spec");
+            iTween.MoveTo(spec, iTween.Hash("y", 470, "delay", 0.1f, "time", 0.5f));
+        }
+        GameManager.instance.spec_check = true;
+
+        for (i = 1; i < 6; i++)
+        {
+            bar = GameObject.Find("gauge" + i.ToString()).GetComponent<Image>();
+            bar.fillAmount = userdata[i - 1] / 100f;
+            gauge = GameObject.Find("gauge_t" + i.ToString()).GetComponent<Text>();
+            gauge.text = userdata[i - 1].ToString() + "/ 100";
+        }
+    }
+    public void specview_h3()
+    {
+        GameManager.instance.select = 2;
+        data = GameManager.instance.select;
+
+        name_t = GameObject.Find("name_t").GetComponent<Text>();
+        name_t.text = GameManager.instance.UserHorse[data].name;
+
+        userdata[0] = GameManager.instance.UserHorse[data].speed;
+        userdata[1] = GameManager.instance.UserHorse[data].accel;
+        userdata[2] = GameManager.instance.UserHorse[data].hp;
+        userdata[3] = GameManager.instance.UserHorse[data].agility;
+        userdata[4] = GameManager.instance.UserHorse[data].consis;
+
+        if (GameManager.instance.spec_check == false)
+        {
+            GameObject spec = GameObject.Find("spec");
+            iTween.MoveTo(spec, iTween.Hash("y", 470, "delay", 0.1f, "time", 0.5f));
+        }
+        GameManager.instance.spec_check = true;
+
+        for (i = 1; i < 6; i++)
+        {
+            bar = GameObject.Find("gauge" + i.ToString()).GetComponent<Image>();
+            bar.fillAmount = userdata[i - 1] / 100f;
+            gauge = GameObject.Find("gauge_t" + i.ToString()).GetComponent<Text>();
+            gauge.text = userdata[i - 1].ToString() + "/ 100";
+        }
+    }
+    public void specview_h4()
+    {
+        GameManager.instance.select = 3;
+        data = GameManager.instance.select;
+
+        name_t = GameObject.Find("name_t").GetComponent<Text>();
+        name_t.text = GameManager.instance.UserHorse[data].name;
+
+        userdata[0] = GameManager.instance.UserHorse[data].speed;
+        userdata[1] = GameManager.instance.UserHorse[data].accel;
+        userdata[2] = GameManager.instance.UserHorse[data].hp;
+        userdata[3] = GameManager.instance.UserHorse[data].agility;
+        userdata[4] = GameManager.instance.UserHorse[data].consis;
+
+        if (GameManager.instance.spec_check == false)
+        {
+            GameObject spec = GameObject.Find("spec");
+            iTween.MoveTo(spec, iTween.Hash("y", 470, "delay", 0.1f, "time", 0.5f));
+        }
+        GameManager.instance.spec_check = true;
+
+        for (i = 1; i < 6; i++)
+        {
+            bar = GameObject.Find("gauge" + i.ToString()).GetComponent<Image>();
+            bar.fillAmount = userdata[i - 1] / 100f;
+            gauge = GameObject.Find("gauge_t" + i.ToString()).GetComponent<Text>();
+            gauge.text = userdata[i - 1].ToString() + "/ 100";
+        }
+    }
+
+    public void specview_h5()
+    {
+        GameManager.instance.select = 4;
+        data = GameManager.instance.select;
+
+        name_t = GameObject.Find("name_t").GetComponent<Text>();
+        name_t.text = GameManager.instance.UserHorse[data].name;
+
+        userdata[0] = GameManager.instance.UserHorse[data].speed;
+        userdata[1] = GameManager.instance.UserHorse[data].accel;
+        userdata[2] = GameManager.instance.UserHorse[data].hp;
+        userdata[3] = GameManager.instance.UserHorse[data].agility;
+        userdata[4] = GameManager.instance.UserHorse[data].consis;
+
+        if (GameManager.instance.spec_check == false)
+        {
+            GameObject spec = GameObject.Find("spec");
+            iTween.MoveTo(spec, iTween.Hash("y", 470, "delay", 0.1f, "time", 0.5f));
+        }
+        GameManager.instance.spec_check = true;
+
+        for (i = 1; i < 6; i++)
+        {
+            bar = GameObject.Find("gauge" + i.ToString()).GetComponent<Image>();
+            bar.fillAmount = userdata[i - 1] / 100f;
+            gauge = GameObject.Find("gauge_t" + i.ToString()).GetComponent<Text>();
+            gauge.text = userdata[i - 1].ToString() + "/ 100";
+        }
+    }
+
+    public void specview_h6()
+    {
+        GameManager.instance.select = 5;
+        data = GameManager.instance.select;
+
+        name_t = GameObject.Find("name_t").GetComponent<Text>();
+        name_t.text = GameManager.instance.UserHorse[data].name;
+
+        userdata[0] = GameManager.instance.UserHorse[data].speed;
+        userdata[1] = GameManager.instance.UserHorse[data].accel;
+        userdata[2] = GameManager.instance.UserHorse[data].hp;
+        userdata[3] = GameManager.instance.UserHorse[data].agility;
+        userdata[4] = GameManager.instance.UserHorse[data].consis;
+
+        if (GameManager.instance.spec_check == false)
+        {
+            GameObject spec = GameObject.Find("spec");
+            iTween.MoveTo(spec, iTween.Hash("y", 470, "delay", 0.1f, "time", 0.5f));
+        }
+        GameManager.instance.spec_check = true;
+
+        for (i = 1; i < 6; i++)
+        {
+            bar = GameObject.Find("gauge" + i.ToString()).GetComponent<Image>();
+            bar.fillAmount = userdata[i - 1] / 100f;
+            gauge = GameObject.Find("gauge_t" + i.ToString()).GetComponent<Text>();
+            gauge.text = userdata[i - 1].ToString() + "/ 100";
+        }
+    }
+    public void gotohome()
+    {
+        SceneManager.LoadScene("mainmap");
+    }
+}
--- marginfarm/Assets/hyeonggook/script/ListSetting.cs
+++ marginfarm/Assets/hyeonggook/script/ListSetting.cs
@@ -9,6 +9,8 @@ public class ListSetting : MonoBehaviour
     public Button button;
     public GameObject x1;
     public GameObject x2;
+    public Text h_name;
+    public GameObject h_image;
 
     public int check;
     public int con;
@@ -23,18 +25,41 @@ public class ListSetting : MonoBehaviour
             button = GameObject.Find("horse" + con.ToString()).GetComponent<Button>();
             GameObject x1 = GameObject.Find("onex" + con.ToString());
             GameObject x2 = GameObject.Find("twox" + con.ToString());
+            h_name = GameObject.Find("h_text" + con.ToString()).GetComponent<Text>();
+            GameObject h_image = GameObject.Find("h_image" + con.ToString());
 
             if (check >= con)
             {
                 button.interactable = true;
+
+                if (GameManager.instance.UserHorse[con - 1].level < 4)
+                {
+                    button.image.color = Color.yellow;
+                }
+                else if (GameManager.instance.UserHorse[con - 1].level < 7)
+                {
+                    button.image.color = Color.green;
+                }
+                else if (GameManager.instance.UserHorse[con - 1].level < 10)
+                {
+                    button.image.color = Color.blue;
+                }
+                else
+                {
+                    button.image.color = Color.red;
+                }
                 x1.SetActive(false);
                 x2.SetActive(false);
+                h_name.text = GameManager.instance.UserHorse[con-1].name;
+                h_image.SetActive(true);
             }
             else
             {
                 button.interactable = false;
                 x1.SetActive(true);
                 x2.SetActive(true);
+                h_name.text = "";
+                h_image.SetActive(false);
             };
 
             //Debug.Log(GameManager.instance.UserHorse[con - 1].level);
@@ -43,6 +68,4 @@ public class ListSetting : MonoBehaviour
     }
 
     // Update is called once per frame
-
-    
 }
