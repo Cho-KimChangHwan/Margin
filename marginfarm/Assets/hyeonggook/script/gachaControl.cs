a6df4e78187f80c5c91b4e5817bf79ea1066a9af
HyeongGook <khg5632@gmail.com>
2022-01-14 01:14:16 +0900

    가챠 1단계 구현

---
+++ marginfarm/Assets/hyeonggook/script/gachaControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gachaControl : MonoBehaviour
+{
+
+    public GameObject first;
+    public GameObject second;
+    public GameObject third;
+
+    public GameObject horse1;
+    public GameObject horse2;
+    public GameObject horse3;
+
+    public void gacha_b1_click()
+    {
+        GameObject first = GameObject.Find("first");
+        GameObject second = GameObject.Find("second");
+        iTween.MoveTo(first, iTween.Hash("y", 1100, "delay", 0.1f, "time", 0.5f));
+        iTween.MoveTo(second, iTween.Hash("y", 360, "delay", 0.1f, "time", 0.5f));
+    }
+    public void go_click()
+    {
+        GameObject horse1 = GameObject.Find("render_horse1"); 
+        GameObject horse2 = GameObject.Find("render_horse2"); 
+        GameObject horse3 = GameObject.Find("render_horse3"); 
+    }
+
+}
