183604c9c019e7387f13b0eff1d55330f872d471
alexjun12 <alexjun12@naver.com>
2022-01-28 01:39:33 +0900

    22_01_28byJun

---
+++ marginfarm/Assets/DbAndServerScripts/charIdGet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class charIdGet : MonoBehaviour
+{
+    public TextMesh charId;
+    public int randd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charId = GetComponent<TextMesh>();
+        randd = Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        charId.text = randd.ToString();
+    }
+}
