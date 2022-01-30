d7e9a144ef72cc996ace6222cb43a034f2b037e5
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2022-01-30 15:14:29 +0900

    22.01.30(TradeScene)

---
+++ marginfarm/Assets/Trade/Script/OpenBuyBoard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBuyBoard : MonoBehaviour
+{
+    public GameObject SellBoard;
+    public GameObject BuyBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuyBoard.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void opener()
+    {
+        SellBoard.gameObject.SetActive(false);
+        BuyBoard.gameObject.SetActive(true);
+    }
+}
