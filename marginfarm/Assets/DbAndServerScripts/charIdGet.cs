f0b4ae3bf2fdb15b8c1ecad33851db1ccc5a968a
alexjun12 <alexjun12@naver.com>
2022-02-03 21:07:29 +0900

    byJun

--- marginfarm/Assets/DbAndServerScripts/charIdGet.cs
+++ marginfarm/Assets/DbAndServerScripts/charIdGet.cs
@@ -18,20 +18,23 @@ public class charIdGet : MonoBehaviourPunCallbacks
 
     void CreateChar()
     {
-        switch(PhotonNetwork.CurrentRoom.PlayerCount)
+        if (photonView.IsMine)
         {
-            case 1:
-                PhotonNetwork.Instantiate("myHorse", new Vector3(34.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
-                break;
-            case 2:
-                PhotonNetwork.Instantiate("myHorse", new Vector3(35.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0); 
-                break;
-            case 3:
-                PhotonNetwork.Instantiate("myHorse", new Vector3(37.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
-                break;
-            case 4:
-                PhotonNetwork.Instantiate("myHorse", new Vector3(38.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
-                break;
+            switch (PhotonNetwork.CurrentRoom.PlayerCount)
+            {
+                case 1:
+                    PhotonNetwork.Instantiate("myHorse", new Vector3(34.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
+                    break;
+                case 2:
+                    PhotonNetwork.Instantiate("myHorse", new Vector3(35.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
+                    break;
+                case 3:
+                    PhotonNetwork.Instantiate("myHorse", new Vector3(37.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
+                    break;
+                case 4:
+                    PhotonNetwork.Instantiate("myHorse", new Vector3(38.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
+                    break;
+            }
         }
     }
 }
