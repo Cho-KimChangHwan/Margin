212b228f7bb6ed1b1a31b81964df52fb82343996
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-02-10 12:39:20 +0900

    ㅇ
    ㅇ

--- marginfarm/Assets/Racing_changh2/HorseStatus.cs
+++ marginfarm/Assets/Racing_changh2/HorseStatus.cs
@@ -142,7 +142,8 @@ public class HorseStatus : MonoBehaviourPunCallbacks
         {
             if (count.isStart)
             {
-                countRecord();
+                if (photonView.IsMine)
+                    countRecord();
                 
                 Run();
             }
