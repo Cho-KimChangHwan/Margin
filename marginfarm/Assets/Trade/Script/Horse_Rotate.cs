9bad64da01020d703d055dac89951be440f8dc24
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2022-02-14 19:32:17 +0900

    1
    1

--- marginfarm/Assets/Trade/Script/Horse_Rotate.cs
+++ marginfarm/Assets/Trade/Script/Horse_Rotate.cs
@@ -8,6 +8,7 @@ public class Horse_Rotate : MonoBehaviour
     private float rotateSpeed = 3f;
     Vector2 mouseposition;
     Camera camera;
+    bool isclick = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,20 +18,36 @@ public class Horse_Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetMouseButton(0))
+        
+        if(Input.GetMouseButton(0))
         {
-           mouseposition = Input.mousePosition;
-           //mouseposition = camera.ScreenToWorldPoint(mouseposition);
-           Debug.Log(mouseposition.ToString());
-           if(mouseposition.x > 1.0f && mouseposition.x<4.0f)
-           {
-               if(mouseposition.y < -0.5f && mouseposition.y >-2.5f)
-               {
-                transform.Rotate(0f, -Input.GetAxis("Mouse X") * rotateSpeed, 0f, Space.World);
-                //transform.Rotate(-Input.GetAxis("Mouse Y") * rotateSpeed, 0f, 0f);
-               }
-           }
+            mouseposition = Input.mousePosition;
+            //mouseposition = camera.ScreenToWorldPoint(mouseposition);
+            Debug.Log(mouseposition.ToString());
+            if(mouseposition.x > 5.0f && mouseposition.x<425.0f)
+            {
+                if(mouseposition.y > 145.0f && mouseposition.y <465.0f)
+                {
+                    Debug.Log("ss");
+                    isclick = true;       
+                }
+            }
         }
+        else
+        {
+            isclick = false;
+        }
+
+        if(isclick == true)
+        {
+            transform.Rotate(0f, -Input.GetAxis("Mouse X") * rotateSpeed, 0f, Space.World);
+            //transform.Rotate(-Input.GetAxis("Mouse Y") * rotateSpeed, 0f, 0f);
+        }
+        
+    }
+
+    public void clickposition()
+    {
         
     }
 
