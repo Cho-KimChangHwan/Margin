fda32257e3eae38c841df6e3cc7ff1373f205c86
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2022-02-14 16:30:06 +0900

    1
    1

--- marginfarm/Assets/Trade/Script/Gohome.cs
+++ marginfarm/Assets/Trade/Script/Gohome.cs
@@ -1,23 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Gohome : MonoBehaviour
 {
+
+    public GameObject fadeImg;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FadeInStart());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
     
     public void gotohome()
     {
-        StartCoroutine("FadeOutStart_main");
+        StartCoroutine(FadeOutStart_main());
+    }
+
+    public IEnumerator FadeInStart()
+    {
+        fadeImg.SetActive(true);
+        for (float f = 1f; f > 0; f -= GameManager.instance.fade_speed)
+        {
+            Color c = fadeImg.GetComponent<Image>().color;
+            c.a = f;
+            fadeImg.GetComponent<Image>().color = c;
+            yield return null;
+        }
+        
+        fadeImg.SetActive(false);
+    }
+    //페이드 인
+    public IEnumerator FadeOutStart_main()
+    {
+        fadeImg.SetActive(true);
+        for (float f = 0f; f < 1; f += GameManager.instance.fade_speed)
+        {
+            Color c = fadeImg.GetComponent<Image>().color;
+            c.a = f;
+            fadeImg.GetComponent<Image>().color = c;
+            yield return null;
+        }
+        SceneManager.LoadScene("aftermain");
     }
 }
