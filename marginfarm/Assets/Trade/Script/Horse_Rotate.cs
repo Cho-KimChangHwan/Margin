3595d8521556b0068d894367984d6fbf5bf9d6a7
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2022-02-14 19:01:34 +0900

    1
    1

--- marginfarm/Assets/Trade/Script/Horse_Rotate.cs
+++ marginfarm/Assets/Trade/Script/Horse_Rotate.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Horse_Rotate : MonoBehaviour
 {
     private float rotateSpeed = 3f;
-    
+    Vector2 mouseposition;
+    Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GameObject.Find("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -18,8 +19,16 @@ public class Horse_Rotate : MonoBehaviour
     {
        if(Input.GetMouseButton(0))
         {
-            transform.Rotate(0f, -Input.GetAxis("Mouse X") * rotateSpeed, 0f, Space.World);
-            //transform.Rotate(-Input.GetAxis("Mouse Y") * rotateSpeed, 0f, 0f);
+           mouseposition = Input.mousePosition;
+           if(mouseposition.x > 1.0f && mouseposition.x<4.0f)
+           {
+               if(mouseposition.y < -0.5f && mouseposition.y >-2.5f)
+               {
+                // mouseposition = camera.ScreenToWorldPoint(mouseposition);
+                transform.Rotate(0f, -Input.GetAxis("Mouse X") * rotateSpeed, 0f, Space.World);
+                //transform.Rotate(-Input.GetAxis("Mouse Y") * rotateSpeed, 0f, 0f);
+               }
+           }
         }
         
     }
