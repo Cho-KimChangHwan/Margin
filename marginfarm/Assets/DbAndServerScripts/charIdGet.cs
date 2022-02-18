feb8295155dc6acbec410eb400d3d3c55e803b4f
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-02-18 23:03:10 +0900

    d

--- marginfarm/Assets/DbAndServerScripts/charIdGet.cs
+++ marginfarm/Assets/DbAndServerScripts/charIdGet.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
@@ -15,17 +15,17 @@ public class charIdGet : MonoBehaviourPunCallbacks
     void Awake()
     {
         PhotonNetwork.IsMessageQueueRunning = true;
-        CreateChar();
+        CreateChar();      
     }
     void CreateChar()
     {
         switch (GameManager.instance.mytern)
         {
             case 1:
-                PhotonNetwork.Instantiate("myHorse1", new Vector3(34.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
+                PhotonNetwork.Instantiate("myHorse1", new Vector3(34.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);                
                 break;
             case 2:
-                PhotonNetwork.Instantiate("myHorse2", new Vector3(35.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
+                PhotonNetwork.Instantiate("myHorse2k", new Vector3(35.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
                 break;
             case 3:
                 PhotonNetwork.Instantiate("myHorse3", new Vector3(37.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
@@ -34,9 +34,9 @@ public class charIdGet : MonoBehaviourPunCallbacks
                 PhotonNetwork.Instantiate("myHorse4", new Vector3(38.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
                 break;
         }
-        //GameManager.instance.lineKey[GameManager.instance.mytern - 1] = GameManager.instance.UserHorse[GameManager.instance.captain].key;
         photonView.RPC("matSet", RpcTarget.AllBuffered, GameManager.instance.mytern - 1, GameManager.instance.UserHorse[GameManager.instance.captain].key);
     }
+
     [PunRPC]
     void matSet(int myline, int matKey)
     {
