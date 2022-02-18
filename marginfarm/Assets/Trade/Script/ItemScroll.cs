9a49311e166c66b730f60d8235f6b7f563e08849
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2022-02-18 20:33:53 +0900

    1
    1

---
+++ marginfarm/Assets/Trade/Script/ItemScroll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScroll : MonoBehaviour
+{
+    public int list = 10;
+    public bool isclick = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        createitem();
+    }
+
+    public void createitem() {
+        if(isclick == true)
+        {
+            int y = 0;
+            for(int i = 0; i<list; i++)
+            {
+                //var node = Instantiate(item,new Vector3(0,y,0),Quaternion.identity);
+                //node.transform.SetParent(GameObject.Find("content").transform);
+                y -= 115;
+            }
+            isclick = false;
+        }
+    }
+}
