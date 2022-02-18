074c328b7ae43113dbfe8cca11664615fb00e9c1
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-02-18 23:05:24 +0900

    d

--- marginfarm/Assets/DbAndServerScripts/charIdGet.cs
+++ marginfarm/Assets/DbAndServerScripts/charIdGet.cs
@@ -25,7 +25,7 @@ public class charIdGet : MonoBehaviourPunCallbacks
                 PhotonNetwork.Instantiate("myHorse1", new Vector3(34.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);                
                 break;
             case 2:
-                PhotonNetwork.Instantiate("myHorse2k", new Vector3(35.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
+                PhotonNetwork.Instantiate("myHorse2", new Vector3(35.5f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
                 break;
             case 3:
                 PhotonNetwork.Instantiate("myHorse3", new Vector3(37.0f, 0f, -12.0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)), 0);
