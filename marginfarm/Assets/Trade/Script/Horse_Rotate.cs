e51befa471f22fff2069240de83b3da9557eeec3
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2022-02-19 19:06:48 +0900

    1
    1

--- marginfarm/Assets/Trade/Script/Horse_Rotate.cs
+++ marginfarm/Assets/Trade/Script/Horse_Rotate.cs
@@ -7,7 +7,7 @@ public class Horse_Rotate : MonoBehaviour
 {
     private float rotateSpeed = 3f;
     Vector2 mouseposition;
-    public Camera camera;
+    //public Camera camera;
     bool isclick = false;
     // Start is called before the first frame update
     void Start()
