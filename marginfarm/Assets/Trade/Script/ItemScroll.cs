ccb62fa47b15272d33f00cd81e4fe722bb8c150f
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2022-02-19 18:47:41 +0900

    22.02.19(Trade)
    SellPanel

--- marginfarm/Assets/Trade/Script/ItemScroll.cs
+++ marginfarm/Assets/Trade/Script/ItemScroll.cs
@@ -4,32 +4,29 @@ using UnityEngine;
 
 public class ItemScroll : MonoBehaviour
 {
-    public int list = 10;
     public bool isclick = false;
     public GameObject item;
+    public GameObject content;
+    //public GameObject noitem;
+    public int yPos = 0;
     // Start is called before the first frame update
+    
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        createitem();
+
     }
 
-    public void createitem() {
-        if(isclick == true)
-        {
-            int y = 0;
-            for(int i = 0; i<list; i++)
-            {
-                var node = Instantiate(item,new Vector3(0,y,0),Quaternion.identity);
-                node.transform.SetParent(GameObject.Find("content").transform);
-                y -= 115;
-            }
-            isclick = false;
-        }
+
+    public void isenrolled(){
+        //noitem.gameObject.SetActive(false);
+        var node = Instantiate(item,content.transform);
+        node.transform.SetParent(GameObject.Find("content").transform);
+        yPos -= 115;       
     }
 }
