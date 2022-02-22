0140a518b9c342092793557cf5647847ed5caf6f
Seochangh2 <nomm89@naver.com>
2022-02-22 22:53:17 +0900

    ㅇㅇ

---
+++ marginfarm/Assets/Racing_changh2/RaceBackSound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RaceBackSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    public AudioClip backSound;
+
+    AudioSource audioSource;
+    float originalV;
+    bool On= true;
+    TextMeshProUGUI soundT;
+
+
+    void Awake() {
+        audioSource = GameObject.Find("SoundManager").GetComponent<AudioSource>();
+        originalV = audioSource.volume;
+        TextMeshProUGUI soundT = GameObject.Find("SoundT").GetComponent<TextMeshProUGUI>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.Play();
+    }
+    public void SoundControl()
+    {
+        if(On)
+        {
+            audioSource.volume = 0f;
+            soundT.text = "Sound ON";
+            On = false;
+        }
+        else
+        {
+            audioSource.volume = originalV;
+            soundT.text = "Sound OFF";
+            On = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
