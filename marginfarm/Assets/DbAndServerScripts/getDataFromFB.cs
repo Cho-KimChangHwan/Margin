f2387e75bd647e30fad5e46819358f667d6e6368
alexjun12 <alexjun12@naver.com>
2022-02-23 13:07:56 +0900

    ...

--- marginfarm/Assets/DbAndServerScripts/getDataFromFB.cs
+++ marginfarm/Assets/DbAndServerScripts/getDataFromFB.cs
@@ -38,24 +38,24 @@ public class getDataFromFB : MonoBehaviour
                 //get first data
                 for (int i = 0; i < snapshot.Child("horses").ChildrenCount; i++)
                 {
-                    GameManager.instance.UserHorse[i].name = snapshot.Child(i.ToString()).Child("name").Value.ToString();
-                    GameManager.instance.UserHorse[i].key = Convert.ToInt32(snapshot.Child(i.ToString()).Child("key").Value);
-                    GameManager.instance.UserHorse[i].level = Convert.ToInt32(snapshot.Child(i.ToString()).Child("level").Value);
-                    GameManager.instance.UserHorse[i].speed = Convert.ToInt32(snapshot.Child(i.ToString()).Child("speed").Value);
-                    GameManager.instance.UserHorse[i].accel = Convert.ToInt32(snapshot.Child(i.ToString()).Child("accel").Value);
-                    GameManager.instance.UserHorse[i].hp = Convert.ToInt32(snapshot.Child(i.ToString()).Child("hp").Value);
-                    GameManager.instance.UserHorse[i].agility = Convert.ToInt32(snapshot.Child(i.ToString()).Child("agility").Value);
-                    GameManager.instance.UserHorse[i].consis = Convert.ToInt32(snapshot.Child(i.ToString()).Child("consis").Value);
-                    GameManager.instance.UserHorse[i].items = Convert.ToInt32(snapshot.Child(i.ToString()).Child("items").Value);
+                    GameManager.instance.UserHorse[i].name = snapshot.Child("horses").Child(i.ToString()).Child("name").Value.ToString();
+                    GameManager.instance.UserHorse[i].key = Convert.ToInt32(snapshot.Child("horses").Child(i.ToString()).Child("key").Value);
+                    GameManager.instance.UserHorse[i].level = Convert.ToInt32(snapshot.Child("horses").Child(i.ToString()).Child("level").Value);
+                    GameManager.instance.UserHorse[i].speed = Convert.ToInt32(snapshot.Child("horses").Child(i.ToString()).Child("speed").Value);
+                    GameManager.instance.UserHorse[i].accel = Convert.ToInt32(snapshot.Child("horses").Child(i.ToString()).Child("accel").Value);
+                    GameManager.instance.UserHorse[i].hp = Convert.ToInt32(snapshot.Child("horses").Child(i.ToString()).Child("hp").Value);
+                    GameManager.instance.UserHorse[i].agility = Convert.ToInt32(snapshot.Child("horses").Child(i.ToString()).Child("agility").Value);
+                    GameManager.instance.UserHorse[i].consis = Convert.ToInt32(snapshot.Child("horses").Child(i.ToString()).Child("consis").Value);
+                    GameManager.instance.UserHorse[i].items = Convert.ToInt32(snapshot.Child("horses").Child(i.ToString()).Child("items").Value);
                 }
                 for (int i = 0; i < snapshot.Child("items").ChildrenCount; i++)
                 {
-                    GameManager.instance.UserItem[i].key = Convert.ToInt32(snapshot.Child("item" + i.ToString()).Child("key").Value);
-                    GameManager.instance.UserItem[i].speed = Convert.ToInt32(snapshot.Child("item" + i.ToString()).Child("speed").Value);
-                    GameManager.instance.UserItem[i].accel = Convert.ToInt32(snapshot.Child("item" + i.ToString()).Child("accel").Value);
-                    GameManager.instance.UserItem[i].hp = Convert.ToInt32(snapshot.Child("item" + i.ToString()).Child("hp").Value);
-                    GameManager.instance.UserItem[i].agility = Convert.ToInt32(snapshot.Child("item" + i.ToString()).Child("agility").Value);
-                    GameManager.instance.UserItem[i].consis = Convert.ToInt32(snapshot.Child("item" + i.ToString()).Child("consis").Value);
+                    GameManager.instance.UserItem[i].key = Convert.ToInt32(snapshot.Child("items").Child("item" + i.ToString()).Child("key").Value);
+                    GameManager.instance.UserItem[i].speed = Convert.ToInt32(snapshot.Child("items").Child("item" + i.ToString()).Child("speed").Value);
+                    GameManager.instance.UserItem[i].accel = Convert.ToInt32(snapshot.Child("items").Child("item" + i.ToString()).Child("accel").Value);
+                    GameManager.instance.UserItem[i].hp = Convert.ToInt32(snapshot.Child("items").Child("item" + i.ToString()).Child("hp").Value);
+                    GameManager.instance.UserItem[i].agility = Convert.ToInt32(snapshot.Child("items").Child("item" + i.ToString()).Child("agility").Value);
+                    GameManager.instance.UserItem[i].consis = Convert.ToInt32(snapshot.Child("items").Child("item" + i.ToString()).Child("consis").Value);
                 }
                 for (int i = 0; i < 24; i++)
                 {
