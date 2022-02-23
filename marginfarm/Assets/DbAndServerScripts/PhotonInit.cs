0de63c737074b3b234c34a4b8a8c66e0d2f251b1
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-02-23 13:37:01 +0900

    ㄴㄴㄴ

--- marginfarm/Assets/DbAndServerScripts/PhotonInit.cs
+++ marginfarm/Assets/DbAndServerScripts/PhotonInit.cs
@@ -21,7 +21,7 @@ public class PhotonInit : MonoBehaviourPunCallbacks
     }
     private void Update()
     {
-        if (getReady == true && PhotonNetwork.CurrentRoom.PlayerCount >=3  && isGameStart == false) //3명일 때
+        if (getReady == true && PhotonNetwork.CurrentRoom.PlayerCount >=4  && isGameStart == false) //3명일 때
         {
             StartCoroutine(this.LoadRacing());
             PhotonNetwork.CurrentRoom.IsOpen = false;
